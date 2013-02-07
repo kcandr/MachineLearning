e1f8c484a495b794b813437b1a6a48ed7b9c452b
kcandr <romanchev.aleksandr@gmail.com>
2013-02-08 01:12:06 +0300

    start

---
+++ ID3/main.cpp
@@ -0,0 +1,30 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int attributeCount = 5;
+const int cortegeCount = 10;
+int table[cortegeCount][attributeCount+1] = {
+    {0, 1, 0, 1, 0, 1},
+    {0, 1, 0, 1, 0, 1},
+    {0, 1, 0, 1, 0, 1},
+    {0, 1, 0, 1, 0, 1},
+    {0, 1, 0, 1, 0, 1},
+    {0, 1, 0, 1, 0, 1},
+    {0, 1, 0, 1, 0, 1},
+    {0, 1, 0, 1, 0, 1},
+    {0, 1, 0, 1, 0, 1},
+    {0, 1, 0, 1, 0, 1}
+}
+
+int gain()
+{
+    return ;
+}
+
+int main()
+{
+    cout << attributeCount;
+    return 1;
+}
