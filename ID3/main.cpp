86a1f22f34f9eca7a33b2051793005843b4f78ac
kcandr <romanchev.aleksandr@gmail.com>
2013-02-09 11:08:20 +0300

    redaing from file

--- ID3/main.cpp
+++ ID3/main.cpp
@@ -1,30 +1,57 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <math.h>
 using namespace std;
 
-const int attributeCount = 5;
-const int cortegeCount = 10;
-int table[cortegeCount][attributeCount+1] = {
-    {0, 1, 0, 1, 0, 1},
-    {0, 1, 0, 1, 0, 1},
-    {0, 1, 0, 1, 0, 1},
-    {0, 1, 0, 1, 0, 1},
-    {0, 1, 0, 1, 0, 1},
-    {0, 1, 0, 1, 0, 1},
-    {0, 1, 0, 1, 0, 1},
-    {0, 1, 0, 1, 0, 1},
-    {0, 1, 0, 1, 0, 1},
-    {0, 1, 0, 1, 0, 1}
+int attributeCount;
+int cortegeCount;
+vector<vector<int> > table;
+
+int read()
+{
+    ifstream input("input.txt");
+    if(!input)
+        return 1;
+    input >> attributeCount;
+    input >> cortegeCount;
+    int tmpInt;
+    vector<int> tmpVector;
+    for (int i = 0; i < cortegeCount; ++i) {
+        for (int j = 0; j < attributeCount; ++j) {
+            input >> tmpInt;
+            tmpVector.push_back(tmpInt);
+        }
+        table.push_back(tmpVector);
+        tmpVector.clear();
+    }
+    return 0;
+}
+
+double entropy()
+{
+    double positive = 0;
+    double negative = cortegeCount;
+
+    for (int i = 0; i < cortegeCount; ++i) {
+        if ( table[i][attributeCount] ) {
+            ++positive;
+        }
+    }
+    negative -= positive;
+    return -(positive / cortegeCount) * (log( positive ) / log( 2.0 )) - 
+        (negative / cortegeCount) * (log( negative ) / log( 2.0 ));
 }
 
 int gain()
 {
-    return ;
+    return 1;
 }
 
 int main()
 {
-    cout << attributeCount;
-    return 1;
+    read();
+    cout << table[0][0];
+    return 0;
 }
