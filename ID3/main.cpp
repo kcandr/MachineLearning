cf8a084ad7ae3d85bf5ac21eb8d1834c53c5c63b
kcandr <romanchev.aleksandr@gmail.com>
2013-02-09 17:15:01 +0300

    Entropy and gain

--- ID3/main.cpp
+++ ID3/main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 int attributeCount;
 int cortegeCount;
+int S;
 vector<vector<int> > table;
 
 int read()
@@ -26,32 +27,57 @@ int read()
         table.push_back(tmpVector);
         tmpVector.clear();
     }
+    S = table.at(0).size() - 1;
     return 0;
 }
 
-double entropy()
+double entropy(const vector<vector<int> > &params, int target)
 {
     double positive = 0;
-    double negative = cortegeCount;
+    double negative = params.size();
 
-    for (int i = 0; i < cortegeCount; ++i) {
-        if ( table[i][attributeCount] ) {
+    int count = params.size();
+    for (int i = 0; i < count; ++i) {
+        if ( params.at(i).at(target) ) {
             ++positive;
         }
     }
     negative -= positive;
-    return -(positive / cortegeCount) * (log( positive ) / log( 2.0 )) - 
-        (negative / cortegeCount) * (log( negative ) / log( 2.0 ));
+    double pk = positive / count;
+    double nk = negative / count;
+
+    if ( positive == 0) {
+        return 0;
+    };
+    if ( negative == 0) {
+        return 1;
+    }
+    return -pk * (log( pk ) / log( 2.0 )) - 
+            nk * (log( nk ) / log( 2.0 ));
 }
 
-int gain()
+double gain(const vector<vector<int> > &params, int Q)
 {
-    return 1;
+    vector<vector<int> > positiveSub;
+    vector<vector<int> > negativeSub;
+    
+    for (int i = 0; i < params.size(); ++i) {
+        if ( params.at(i).at(Q) ) {
+            positiveSub.push_back(params.at(i));
+        } else {
+            negativeSub.push_back(params.at(i));
+        }
+    }
+    double pk = (double) positiveSub.size() / params.size();
+    double nk = (double) negativeSub.size() / params.size();
+
+    return entropy(params, S) - pk * entropy(positiveSub, S) 
+                              - nk * entropy(negativeSub, S);
 }
 
 int main()
 {
     read();
-    cout << table[0][0];
+    cout << gain(table, 1);
     return 0;
 }
